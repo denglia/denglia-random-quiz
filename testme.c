efb04a0ad85cdc47c4cce50b4a9bb9b28e6d5e91
denglia <46579602+denglia@users.noreply.github.com>
2019-11-11 15:50:10 +0800

    Add files via upload

---
+++ testme.c
@@ -0,0 +1,73 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
+
+char inputChar()
+{
+    // TODO: rewrite this function
+    int low = 0;
+    int high = 99;
+    int i = 0;
+    for (i = 0; i < count; i++){
+      int num = (rand()%(high-low +1))+low;
+    }
+
+    char test = num;
+    return test;
+}
+
+char *inputString()
+{
+    // TODO: rewrite this function
+    char input[7];
+    int i = 0;
+    int low = 11;
+    int high = 111;
+    for(i=0; i < 5; i++){
+      input[i] = (rand()%(high-low +1))+low;
+    }
+    return input;
+}
+
+void testme()
+{
+  int tcCount = 0;
+  srand(time(0));
+  char *s;
+  char c;
+  int state = 0;
+  while (1)
+  {
+    tcCount++;
+    c = inputChar();
+    s = inputString();
+    printf("Iteration %d: c = %c, s = %s, state = %d\n", tcCount, c, s, state);
+
+    if (c == '[' && state == 0) state = 1;
+    if (c == '(' && state == 1) state = 2;
+    if (c == '{' && state == 2) state = 3;
+    if (c == ' '&& state == 3) state = 4;
+    if (c == 'a' && state == 4) state = 5;
+    if (c == 'x' && state == 5) state = 6;
+    if (c == '}' && state == 6) state = 7;
+    if (c == ')' && state == 7) state = 8;
+    if (c == ']' && state == 8) state = 9;
+    if (s[0] == 'r' && s[1] == 'e'
+       && s[2] == 's' && s[3] == 'e'
+       && s[4] == 't' && s[5] == '\0'
+       && state == 9)
+    {
+      printf("error ");
+      exit(200);
+    }
+  }
+}
+
+
+int main(int argc, char *argv[])
+{
+    srand(time(NULL));
+    testme();
+    return 0;
+}
